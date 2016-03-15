994d128afc4fe00f9970af88f636a7d19cba0de7
Ross <rssfltchr@gmail.com>
2016-03-15 16:50:06 +0100

    Initial add

---
+++ src/DoctorDetails.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ross on 3/15/16.
+ */
+
+
+public class DoctorDetails {
+    String firstName;
+    String lastName;
+    String department;
+
+
+    public DoctorDetails(String firstName, String lastName, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+    }
+}
---
+++ src/NurseDetails.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ross on 3/15/16.
+ */
+public class NurseDetails {
+}
